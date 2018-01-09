762fc347d5adf513e8df787283badd75835a46e7
Elvin Yeung <elvinyeung@gmail.com>
2018-01-09 00:04:37 -0500

    semi functional speedo

--- src/client/app/render.jsx
+++ src/client/app/render.jsx
@@ -3,6 +3,10 @@ import {render} from 'react-dom';
 import Speedo from './speedo.jsx';
 import FuelGauge from './fuelGauge.jsx';
 
+	//TODO:  this is for testing only.  real speed need to be passed in.   	
+	setTimeout(function(){
+		Speedo.setSpeed(Math.random()*100);
+	}.bind(this), 500); 
 
 render(<Speedo/>, document.getElementById('speedo'));
 render(<FuelGauge/>, document.getElementById('fuelGauge'));
--- src/client/app/speedo.jsx
+++ src/client/app/speedo.jsx
@@ -1,15 +1,47 @@
+/*
+ credit to: Stefan Beutler, I copy his work from https://codepen.io/Sm1lEE/pen/IhbAy,  and heavily modify to suit my need. 
+*/
+
 import React from 'react';
 import {render} from 'react-dom';
 import './speedo.css';
 
 export default class Speedo extends React.Component {
 
+  constructor(props) {
+    super(props);
+	this.state = {
+      speed: "0MPH",
+      degree: 0
+    };   
+
+	//TODO:  this is for testing only.  real speed need to be passed in.   	
+	setInterval(function(){
+		console.log("setting speed");
+		this.setSpeed(Math.random()*100);
+	}.bind(this), 5000); 
+
+  }
+
   render () {
+
+
+    const styles = { 
+        transform: `rotate(${this.state.degree}deg)` 
+    };
     return (
-		<div id="speedometer" data-value="0">
-  			<span id="speedoNeedle"></span>
+		<div id="speedometer" data-value={this.state.speed}>
+  			<span id="speedoNeedle" style={styles}></span>
 		</div>
     );
   }
+
+  setSpeed (speed) {
+  	this.setState({
+  		speed: Math.round(speed)+"MPH", 
+  		degree: Math.round(speed*3/2)
+  	});
+  }
+
 }
 
