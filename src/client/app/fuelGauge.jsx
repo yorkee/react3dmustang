3e31961effe5ee66fa89f7e5827358e345ae48a6
Elvin Yeung <elvinyeung@gmail.com>
2018-01-12 04:13:25 -0500

    a bit of styles

--- src/client/app/fuelGauge.jsx
+++ src/client/app/fuelGauge.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+import './gauge.css'
 
 export default class FuelGauge extends React.Component {
   render () {
     return (
-      <div>
+      <div className='gauge'>
         <p> Full | Empty</p>
       </div>
     );
